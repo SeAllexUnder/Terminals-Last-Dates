37bceb8c56498cc98a36514ef43945432b7b7fe6
Alexander <sun5038@yandex.ru>
2023-10-24 11:50:48 +0300

    Добавлено определение имени ТС в соответствии с "Информация по ТС"

--- main.py
+++ main.py
@@ -45,6 +45,15 @@ def main():
     last_dates = read_directory('D:\Back up')
     # print('Архивы считаны!\n\n')
     rows = []
+    gs = gspread.service_account('refuelings-0679028911f4.json')
+    table = gs.open_by_key('1CTYD6IilWWe-3MMgALWBYwSpFg-3a0kbaAG9wB_bMRc')
+    info_sheet = table.worksheet('Информация по ТС')
+    info_values = info_sheet.get_values()
+    info_vehicles = {}
+    for val in info_values:
+        if val[1] == '' or val[1] == 'IMEI':
+            continue
+        info_vehicles[val[1]] = val[0]
     for ls in last_dates:
         # print(ls)
         client = ls.split('/')[-2]
@@ -54,26 +63,25 @@ def main():
             first_date = datetime.fromtimestamp(min(last_dates[ls][terminal])).strftime('%d.%m.%Y %H:%M:%S')
             last_date = datetime.fromtimestamp(max(last_dates[ls][terminal])).strftime('%d.%m.%Y %H:%M:%S')
             count = len(last_dates[ls][terminal])
+            vehicle_name = 'Не определено'
+            try:
+                vehicle_name = info_vehicles[terminal]
+            except KeyError:
+                pass
             row = [
-                client,
-                port,
+                str(client),
+                str(port),
                 terminal,
+                vehicle_name,
                 first_date,
                 last_date,
                 count
             ]
             rows.append(row)
-    gs = gspread.service_account('refuelings-0679028911f4.json')
-    table = gs.open_by_key('1CTYD6IilWWe-3MMgALWBYwSpFg-3a0kbaAG9wB_bMRc')
     worksheet = table.worksheet('Снятие данных с терминалов')
     row_count = worksheet.row_count
-    worksheet.batch_clear([f'A2:F{row_count}'])
+    worksheet.batch_clear([f'A2:G{row_count}'])
     worksheet.update(f'A2', rows)
-    # worksheet.columns_auto_resize(0, 6)
-    # table = Google_Sheets.Google_Sheets('1CTYD6IilWWe-3MMgALWBYwSpFg-3a0kbaAG9wB_bMRc', glob.glob('refuelings-0679028911f4.json')[0])
-    # table.clear_sheet('Снятие данных с терминалов', colcount=7)
-    # table.append_rows('Снятие данных с терминалов', 'A1', rows)
-    # table.autoChange_size_columns('Снятие данных с терминалов')
 
 
 if __name__ == '__main__':
